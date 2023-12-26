25911a2e53f21c9c53098e2b283a51619c5b1e98
Victor <47047497+173716414@users.noreply.github.com>
2023-12-26 11:09:00 +0800

    Day13 Redis Tamplate Spring

---
+++ redis-demo/src/test/java/com/victor/RedisDemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.victor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+class RedisDemoApplicationTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    void testString() {
+        redisTemplate.opsForValue().set("name", "vic");
+        Object name = redisTemplate.opsForValue().get("name");
+        System.out.println(name);
+    }
+
+}
---
+++ 笔记/Day13.md
@@ -0,0 +1,7 @@
+## Day13
+
+### redisTamplate
+
+1. 引入以来
+2. 配置redis信息
+3. 注入redisTamplate并使用
